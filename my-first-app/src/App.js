1db49d43e61eeb6423dc3918a1b3acc07a3b3b9b
jianhua3296 <tuk92253@temple.edu>
2020-09-24 19:45:15 -0400

    conditional rendering of component
    2 ways to do it:
    
    1, if-else
    2. logical &&, for example,
    a && statement, if a is true, statement will be executed. really?

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NameTag from "./components/nameTag";
-import NameTag2 from "./components/nameTag2"
+import NameTag2 from "./components/nameTag2";
+import NameTag3 from "./components/nameTag3"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Name List</h1>
+        {/* passing first and last name as prob member variables, component destruct them and get their values respectively */}
         <NameTag firstName="jianhua" lastName="Code"></NameTag>
+        {/* passing name as prob children  */}
         <NameTag2 >Jian</NameTag2>
         <NameTag2>Faker</NameTag2>
-
+        {/* conditional rendering */}
+        <NameTag3></NameTag3>
+        <NameTag3 firstName="john"></NameTag3>
+      
 
       </header>
     </div>
---
+++ my-first-app/src/components/nameTag3.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+//handle conditioanl rendering
+
+export default (prop) => {
+  if (!prop.firstName && !prop.lastName) {
+
+    console.log(prop.firstName);
+    return (
+      <>
+        <h3 className="name">No First name</h3>
+        <h3 className="name">No last name</h3>
+      </>
+    );
+  }
+  return (
+    <>
+      <h3 className="name">{prop.firstName}</h3>
+      <h3 className="name">{prop.lastName}</h3>
+      {/* logical &&, right side will execute if the left side is true */}
+      {prop.firstName==="john" && <div style={{color:"green"}}>VIP</div>}
+    </>
+  );
+};
